5ffdacde84fa712b11b7f298c2f0822d742ca411
christophe lecomte <leatherface88510@gmail.com>
2020-04-15 09:41:01 +0200

    utilastion d'un fichier template.php pour générer les pages

--- indexView.php
+++ indexView.php
@@ -1,35 +1,30 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Mon super blog</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<h1>Mon super blog!</h1>
-Derniers billet du blog:
+<?php $title = 'Mon blog'; ?>
+
+<?php ob_start(); ?>
+<h1>Mon super blog !</h1>
+<p>Derniers billets du blog :</p>
+
+
 <?php
 while ($data = $posts->fetch())
 {
     ?>
     <div class="news">
         <h3>
-            <?= htmlspecialchars($data['title']); ?>
-            <em>le <?= $data['creation_date_fr']; ?></em>
+            <?= htmlspecialchars($data['title']) ?>
+            <em>le <?= $data['creation_date_fr'] ?></em>
         </h3>
+
         <p>
-            <?= nl2br(htmlspecialchars($data['content'])); ?>
+            <?= nl2br(htmlspecialchars($data['content'])) ?>
             <br />
-            <em><a href="post.php?id=<?= $data['id'] ; ?>">Commentaires</a></em>
+            <em><a href="post.php?id=<?= $data['id'] ?>">Commentaires</a></em>
         </p>
-
     </div>
-
     <?php
 }
 $posts->closeCursor();
 ?>
+<?php $content = ob_get_clean(); ?>
 
-
-</body>
-</html>
+<?php require('template.php'); ?>
--- postView.php
+++ postView.php
@@ -1,36 +1,35 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Commentaires</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<h1>Mon super blog!</h1>
-<a href="index.php">Retour à la liste des billets</a>
 
-            <div class="news">
-                <h3>
-                    <?= htmlspecialchars($post['title']) ?>
-                    <em>le <?= $post['creation_date_fr'] ?></em>
-                </h3>
+<?php $title = 'Commentaires'; ?>
 
-                <p>
-                    <?= nl2br(htmlspecialchars($post['content'])) ?>
-                </p>
-            </div>
-            <div class="commentaires">
-                <h2>Commentaires</h2>
+<?php ob_start(); ?>
+    <h1>Mon super blog!</h1>
+    <a href="index.php">Retour à la liste des billets</a>
 
-                <?php
-                while ($comment = $comments->fetch())
-                {
-                    ?>
-                    <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-                    <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-                    <?php
-                }
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($post['title']) ?>
+            <em>le <?= $post['creation_date_fr'] ?></em>
+        </h3>
+
+        <p>
+            <?= nl2br(htmlspecialchars($post['content'])) ?>
+        </p>
+    </div>
+    <div class="commentaires">
+        <h2>Commentaires</h2>
+
+        <?php
+            while ($comment = $comments->fetch())
+            {
                 ?>
-            </div>
-</body>
-</html>
+                <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+                <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+            <?php
+            }
+        $comments->closeCursor();
+            ?>
+    </div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
